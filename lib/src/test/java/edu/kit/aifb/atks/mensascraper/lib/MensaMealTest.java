f68dbb06cc95e7419ae33b05a2924518ded4bb54
Ferdinand Mütsch <ferdinand@muetsch.io>
2023-05-22 09:41:07 +0200

    fix: test list immutability

--- lib/src/test/java/edu/kit/aifb/atks/mensascraper/lib/MensaMealTest.java
+++ lib/src/test/java/edu/kit/aifb/atks/mensascraper/lib/MensaMealTest.java
@@ -2,17 +2,26 @@ package edu.kit.aifb.atks.mensascraper.lib;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MensaMealTest {
 
     @Test
     public void testCopy() {
-        var m1 = MensaMeal.builder().name("Meal 1").price(1.99f).line(MensaLine.LINIE_1).type(MensaMealType.VEGAN).build();
+        var m1 = MensaMeal.builder()
+                .name("Meal 1")
+                .price(1.99f)
+                .line(MensaLine.LINIE_1)
+                .type(MensaMealType.VEGAN)
+                .additives(List.of("A"))
+                .build();
         var m2 = m1.copy();
         m1.setName("Meal 2");
         m1.setPrice(2.99f);
         m1.setLine(MensaLine.LINIE_2);
+        m1.setAdditives(List.of("B"));
         m2.setType(MensaMealType.VEGETARIAN);
         assertEquals("Meal 2", m1.getName());
         assertEquals("Meal 1", m2.getName());
@@ -20,6 +29,8 @@ public class MensaMealTest {
         assertEquals(1.99f, m2.getPrice());
         assertEquals(MensaLine.LINIE_2, m1.getLine());
         assertEquals(MensaLine.LINIE_1, m2.getLine());
+        assertEquals("B", m1.getAdditives().get(0));
+        assertEquals("A", m2.getAdditives().get(0));
         assertEquals(MensaMealType.VEGAN, m1.getType());
         assertEquals(MensaMealType.VEGETARIAN, m2.getType());
     }
